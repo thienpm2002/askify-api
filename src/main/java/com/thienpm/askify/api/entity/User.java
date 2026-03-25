4a1b75eee3c06c0b0bbeaecc748ca07b7bb37cc0
Thien <ledinhthienpm@gmail.com>
2026-03-25 10:11:50 +0700

    fix: match entity id type with database INT column

--- src/main/java/com/thienpm/askify/api/entity/User.java
+++ src/main/java/com/thienpm/askify/api/entity/User.java
@@ -31,7 +31,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false, unique = true)
     private String email;
