415540071ea45fb10dba7c8807641c4113367d36
Thien <ledinhthienpm@gmail.com>
2026-03-30 17:13:43 +0700

    chore: config upload properties && add web config for static resource handler

---
+++ src/main/java/com/thienpm/askify/api/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.thienpm.askify.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final UploadProperties uploadProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + uploadProperties.getDir() + "/");
+    }
+}
