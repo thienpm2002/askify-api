6bc1455992b9ae96b6c3f03614cc403945a4f446
Thien <ledinhthienpm@gmail.com>
2026-03-30 18:04:12 +0700

    feat: add feature update avatar

--- src/main/java/com/thienpm/askify/api/service/user/UserService.java
+++ src/main/java/com/thienpm/askify/api/service/user/UserService.java
@@ -1,5 +1,7 @@
 package com.thienpm.askify.api.service.user;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.thienpm.askify.api.dto.request.UpdateProfileRequest;
 import com.thienpm.askify.api.dto.response.UserProfileResponse;
 import com.thienpm.askify.api.security.user.CustomUserDetails;
@@ -9,4 +11,6 @@ public interface UserService {
     UserProfileResponse getProfile(CustomUserDetails userDetails);
 
     UserProfileResponse updateProfile(UpdateProfileRequest updateProfileRequest, CustomUserDetails userDetails);
+
+    UserProfileResponse updateAvatar(MultipartFile avatarFile, CustomUserDetails userDetails);
 }
