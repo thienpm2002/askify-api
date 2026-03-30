07409055e45acd2c1b8cbed98c9a59dd94d289c4
Thien <ledinhthienpm@gmail.com>
2026-03-30 11:59:12 +0700

    feat: add feature update user profile

---
+++ src/main/java/com/thienpm/askify/api/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,14 @@
+package com.thienpm.askify.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+    @NotBlank(message = "Username is required")
+    private String userName;
+}
