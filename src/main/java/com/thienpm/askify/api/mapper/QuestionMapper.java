cd4fed1f8dd5bdeef8e9de3ffbb0906b9dd555a7
Thien <ledinhthienpm@gmail.com>
2026-03-31 22:08:58 +0700

    feat: add feature create new question

---
+++ src/main/java/com/thienpm/askify/api/mapper/QuestionMapper.java
@@ -0,0 +1,26 @@
+package com.thienpm.askify.api.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.thienpm.askify.api.dto.response.AuthorQuestionResponse;
+import com.thienpm.askify.api.dto.response.QuestionResponse;
+import com.thienpm.askify.api.entity.Question;
+import com.thienpm.askify.api.entity.Tag;
+import com.thienpm.askify.api.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface QuestionMapper {
+    @Mapping(target = "author", source = "user")
+    @Mapping(target = "tags", source = "tags")
+    QuestionResponse toResponse(Question question);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "userName", source = "userName")
+    @Mapping(target = "avatarUrl", source = "avatarUrl")
+    AuthorQuestionResponse toAuthorResponse(User user);
+
+    default String tagToString(Tag tag) {
+        return tag.getName();
+    }
+}
---
+++ src/main/java/com/thienpm/askify/api/service/question/QuestionService.java
@@ -0,0 +1,9 @@
+package com.thienpm.askify.api.service.question;
+
+import com.thienpm.askify.api.dto.request.CreateQuestionRequest;
+import com.thienpm.askify.api.dto.response.QuestionResponse;
+import com.thienpm.askify.api.security.user.CustomUserDetails;
+
+public interface QuestionService {
+    QuestionResponse createQuestion(CreateQuestionRequest questionRequest, CustomUserDetails userDetails);
+}
