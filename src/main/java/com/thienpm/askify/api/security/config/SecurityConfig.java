958d25f1ec737140111310b86a2b6743c5eee9b7
Thien <ledinhthienpm@gmail.com>
2026-05-11 15:05:14 +0700

    feat: add config CORS

--- src/main/java/com/thienpm/askify/api/security/config/SecurityConfig.java
+++ src/main/java/com/thienpm/askify/api/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.thienpm.askify.api.security.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,6 +14,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.Customizer;
 
 import com.thienpm.askify.api.dto.response.ErrorResponse;
 import com.thienpm.askify.api.enums.ErrorCode;
@@ -28,10 +34,38 @@ public class SecurityConfig {
 
         private final JwtAuthenticationFilter jwtAuthFilter;
 
+        // Cấu hình CORS để frontend có thể gọi API
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+
+                configuration.setAllowedOrigins(List.of(
+                                "http://localhost:5173"));
+
+                configuration.setAllowedMethods(List.of(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "PATCH",
+                                "OPTIONS"));
+
+                configuration.setAllowedHeaders(List.of("*"));
+
+                configuration.setAllowCredentials(true);
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+                source.registerCorsConfiguration("/**", configuration);
+
+                return source;
+        }
+
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http)
                         throws Exception {
                 http
+                                .cors(Customizer.withDefaults())
                                 // 1. Tắt CSRF — REST API dùng JWT không cần CSRF
                                 .csrf(csrf -> csrf.disable())
 
